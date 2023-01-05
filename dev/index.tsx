f5d25c8d38128d9c2f3e0b6e66e776c92ba398ae
OrJDev <orjdeveloper@gmail.com>
2023-01-05 19:26:44 +0200

    get devtools to actually work

--- dev/index.tsx
+++ dev/index.tsx
@@ -1,6 +1,15 @@
 import { render } from "solid-js/web";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
 import "./styles.css";
-
 import App from "./App";
 
-render(() => <App />, document.getElementById("root")!);
+const queryClient = new QueryClient();
+
+render(
+  () => (
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  ),
+  document.getElementById("root")!,
+);
